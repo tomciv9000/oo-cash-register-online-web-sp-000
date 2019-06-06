8a2438d6791acc29d7dce35198a9f4543134c419
Tom White <eugene.white@gmail.com>
2019-06-06 04:21:36 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,7 +10,7 @@ class CashRegister
   end 
   
   def add_item(title, price, quantity = 1)
-    self.total =+ price * quantity
+    @total =+ price * quantity
     quantity.times do
       items << title
     end
